9916c1a3e86b4c47c1378d201c147c3fabc1cf1c
replyrebuff <44467554+replyrebuff@users.noreply.github.com>
2023-01-29 12:30:20 +0100

    added prettier and eslint

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,6 +1,6 @@
 export interface Course {
-	courseName: string;
-	testCode: string;
-	date: string;
-	grade: string;
+    courseName: string;
+    testCode: string;
+    date: string;
+    grade: string;
 }
