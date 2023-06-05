5d015e7216617d41dffba3c25c5b11cee9caa4e2
sipacid <44467554+sipacid@users.noreply.github.com>
2023-06-05 08:39:31 +0200

    import formatting, code formatting

--- bot/src/interfaces.ts
+++ bot/src/interfaces.ts
@@ -1,6 +1,6 @@
 export interface Course {
-    courseName: string;
-    testCode: string;
-    date: Date;
-    grade: string;
+	courseName: string;
+	testCode: string;
+	date: Date;
+	grade: string;
 }
